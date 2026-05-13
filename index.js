e774fdeaa53dc68b60b51429016bbe803afdce18
Ehab-Waleed <xehabwaleed@gmail.com>
2026-05-13 04:53:54 +0300

    solve Part 1.2

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ let x = "123"
 console.log(Number(x) + 7);
 // *********************************** // 
 
+// Part 1.2 
+
+let value = 0 
+if(value == false){
+     console.log("Invalid");
+}
+// *********************************** // 
+
 
 
  
