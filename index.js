fd997e3083d080131210e88e0468ba910280ac57
Ehab-Waleed <xehabwaleed@gmail.com>
2026-05-13 05:09:52 +0300

    solve Part 1.6

--- index.js
+++ index.js
@@ -40,7 +40,34 @@ if(element % 2 ==0){
  console.log(arr3);
   // *********************************** // 
 
-  
+  // Part 1.6 
+
+  let numOfDay = 2
+  switch(numOfDay){
+    case (1) :
+        console.log("Sunday");
+        break
+    case (2) : 
+        console.log("Monday");
+        break
+    case (3) :
+        console.log("Tuesday");
+        break
+    case (4) :
+        console.log("Wednesday");
+        break
+    case (5) :
+        console.log("Thursday");
+        break
+    case (6) :
+        console.log("Friday");
+        break
+    case (7) :
+        console.log("Saturday");
+        break
+  }
+    // *********************************** // 
+
 
 
 
